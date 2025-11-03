f1dd6c07fc3bbfb7da6dc2b88429d41ec1b1bc6c
Isobe Kazuhiko <179515+muramasa64@users.noreply.github.com>
2025-11-03 16:49:09 +0900

    zenn-cliをflakeで管理
    不要ファイルを削除

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "zenn-cli";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            zenn-cli
+          ];
+
+          shellHook = ''
+            echo "zenn-cli version: $(zenn --version)"
+          '';
+        };
+      }
+    );
+}
